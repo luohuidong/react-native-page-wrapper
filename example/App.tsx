f2b002c9f6ab8b9eb21061caf8a0c43e4ed3826d
luohuidong <luohuidong01@126.com>
2020-01-16 22:12:07 +0800

    Add the example.

---
+++ example/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import A from '@luohuidong/react-native-page-wrapper/'
+console.log("TCL: A", A)
+
+function App() {
+  return (
+    <A>
+    </A>
+  )
+}
+
+export default App;
